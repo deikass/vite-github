2b020bbd15c25df6da60859b4e87b4e2a7cd618c
Deividas Kasperavičius <deividas.kasperavicius@nfq.com>
2023-10-02 09:54:35 +0300

    Added logic for empty row group

--- table.js
+++ table.js
@@ -1,9 +1,9 @@
-const toggleRowExpansion = (rows, collapseButton, expandButton, rowGroup, expandRowGroup) => {
+const toggleRowExpansion = (nestedRows, collapseButton, expandButton, rowGroup, expandRowGroup) => {
     rowGroup.classList.toggle('is-active');
     collapseButton.classList.toggle('d-none', !expandRowGroup);
     expandButton.classList.toggle('d-none', expandRowGroup);
 
-    rows.forEach((row) => {
+    nestedRows.forEach((row) => {
         row.classList.toggle('d-none', !expandRowGroup);
     });
 }
@@ -17,15 +17,19 @@ const initializeTable = (table) => {
         const headerRow = rowGroup.querySelector('[data-header-row]');
         const expandButton = headerRow?.querySelector('[data-expand-button]');
         const collapseButton = headerRow?.querySelector('[data-collapse-button]');
-        const rows = rowGroup.querySelectorAll('tr:not([data-header-row])');
+        const nestedRows = rowGroup.querySelectorAll('tr:not([data-header-row])');
 
         if (!headerRow || !expandButton || !collapseButton) {
             console.warn("Incomplete data in row group.");
             return;
         }
 
-        expandButton.addEventListener("click", () => toggleRowExpansion(rows, collapseButton, expandButton, rowGroup, true));
-        collapseButton.addEventListener("click", () => toggleRowExpansion(rows, collapseButton, expandButton, rowGroup, false));
+        if (nestedRows.length === 0) return;
+
+        expandButton.classList.toggle('d-none')
+
+        expandButton.addEventListener("click", () => toggleRowExpansion(nestedRows, collapseButton, expandButton, rowGroup, true));
+        collapseButton.addEventListener("click", () => toggleRowExpansion(nestedRows, collapseButton, expandButton, rowGroup, false));
     });
 }
 
